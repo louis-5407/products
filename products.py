982ff8e54b2eeae6000900a4c66ca6149b85aa26
louis liao <louis.5407@gmail.com>
2023-08-20 12:00:27 +0800

    second_commit

--- products.py
+++ products.py
@@ -14,4 +14,12 @@ print(products)
 
 for product in products:
 	print(product[0], "價格是", product[1])
-	
+
+
+with open("products.csv", "w") as f:
+	for product in products:
+		f.write(product[0] + "," + product[1] + "\n")
+
+
+
+
